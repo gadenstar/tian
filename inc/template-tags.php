b14eaaea170be18dea16c0db7a54d47a85bedc8c
TanX <3510926@qq.com>
2015-05-05 01:24:08 +0800

    移除后台W图标
    移除后台Wordpress图标

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -522,4 +522,10 @@ function nii_comment($comment, $args, $depth) {
         }
 
 
+function annointed_admin_bar_remove() {
+        global $wp_admin_bar;
+        $wp_admin_bar->remove_menu('wp-logo');
+}
+add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
+
 
